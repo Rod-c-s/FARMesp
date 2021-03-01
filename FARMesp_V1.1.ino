507065def00c476e1e32a36aae3dae9c4053936f
Rod-c-s <67756632+Rod-c-s@users.noreply.github.com>
2021-02-28 23:26:50 -0300

    Update FARMesp_V1.1.ino

--- FARMesp_V1.1.ino
+++ FARMesp_V1.1.ino
@@ -1,9 +1,12 @@
-/* FARMesp -- utilizando funções -------------------------------
+/* FARMesp ------------------------------------------------------------
   D0=16; D1=5; D2=4; D3=0; D4=2; D5=14; D6=12; D7=13; D8=15; A0=17
+-----------------------------------------------------------------------
+Desenvolvido por Rodrigo Carvalho
+Rod-c-s
+FARMesp V1.1
+28/02/2021
 
-
-
-*/
+---------------------------------------------------------------------*/
 
 #include <ESP8266WiFi.h>
 #include<ESP8266WebServer.h>
@@ -21,32 +24,33 @@ char* pass = "*******";//armazena a senha da sua rede (substitua os **** pela su
 
 //constantes de portas
 const int sensorSolo = 17;//informa qual sera a constante da porta de entrada do sensor
-const int bomba = 5;//informa qual sera a constante da porta da bomba de agua 
-const int vccSensor = 16;// informa qual sera a constante da porta 
+const int bomba = 5;//informa qual sera a constante da porta da bomba de agua
+const int vccSensor = 16;// informa qual sera a constante da porta
 
-//  consts
-const int porcent = 4.74;// armazena o valor correspondente para obter a porcentagem a partir da leitura do sensor 
-
+//  consts numericas
+const int porcent = 4.74;// armazena o valor correspondente para obter a porcentagem a partir da leitura do sensor
+const int segundos = 1000;// armazena o valor correspondente para um segundo em ms
+const int minuto = 60000;// armazena o valor correspondente para um minuto em ms
 
 //=================================================================================================================//
 //
 //   EDITE OS VALORES ABAIXO PARA DEFINIR AS CONFIGURAÇÕES DO SOLO
+//
 //=================================================================================================================//
 
-        int valorMin = 25; // valor em porcentagem
-        
-//=================================================================================================================//
-
-       int tempoIrrigando = 7;// Tempo em segundos que a bomba fica ligada a cada vez que a umidade do solo for detectada como baixa
-   
-//=================================================================================================================//
-
-      int tempoEntIrrigacao = 15; // Tempo em minutos entre verificações 
+int valorMin = 25; // valor em porcentagem
 
 //=================================================================================================================//
 
+int tempoIrrigando = 6;// Tempo em segundos que a bomba fica ligada a cada vez que a umidade do solo for detectada como baixa
 
-       
+//=================================================================================================================//
+
+int tempoEntIrrigacao = 1; // Tempo em minutos entre verificações
+
+//=================================================================================================================//
+
+
 
 //floats
 float umidade = 0;//umidade do ar
@@ -59,32 +63,30 @@ DHT dht(DHTpin, DHTTYPE);
 
 
 /*=================================================================================================================/
- +++   CASO QUEIRA REGISTRAR OS DADOS OBTIDOS PELO GOOGLE SHEETS (EXCEL DA GOOGLE) UTILIZANDO O GOOGLE FORMS:    +++
-parte 1:
-1º Crie um planilha no google drive(indentificando com o nome para facilitar a identificação
-2º Crie um Forms no google drive
-3º Configure as peguntas para recebam respostas de forma discertativa (Resposta curta)
-4º Vá em "Respostas" e selecione o destino da resposta como a planilha criada anteiormente
+  +++   CASO QUEIRA REGISTRAR OS DADOS OBTIDOS PELO GOOGLE SHEETS (EXCEL DA GOOGLE) UTILIZANDO O GOOGLE FORMS:    +++
+  parte 1:
+  1º Crie um planilha no google drive(indentificando com o nome para facilitar a identificação
+  2º Crie um Forms no google drive
+  3º Configure as peguntas para recebam respostas de forma discertativa (Resposta curta)
+  4º Vá em "Respostas" e selecione o destino da resposta como a planilha criada anteiormente
 
-parte 2:
-5ºAbra o formulário
-6ºClique com o direito e selecione "Exibir código fonte da pagina" 
-7ºutilize a ferramenta de pesquisa no navegador(Ctrl+F) e pesquise o nome do campo do fomulario
-8ºEncontre o código parecido com esse: 
+  parte 2:
+  5ºAbra o formulário
+  6ºClique com o direito e selecione "Exibir código fonte da pagina"
+  7ºutilize a ferramenta de pesquisa no navegador(Ctrl+F) e pesquise o nome do campo do fomulario
+  8ºEncontre o código parecido com esse:
     FB_PUBLIC_LOAD_DATA_ = [null,[null,[[490158642,”Nome do campo“,null,0,[[NumeroDoCampo,null,0,null,[[1,9,[“”];
-9ºCopie esses numeros(NumeroDoCampo) do formulario e insira no valor correspondente na String abaixo (********)
+  9ºCopie esses numeros(NumeroDoCampo) do formulario e insira no valor correspondente na String abaixo (********)
 
-10º Verifique se os dados informados correspondem aos dos campos abaixo
+  10º Verifique se os dados informados correspondem aos dos campos abaixo
 
-/=================================================================================================================*/
-
-
+  /=================================================================================================================*/
 
 String forms1 = "GET /forms/d/e/1FAIpQLSdz5nJ9VMvICIn8jXRHdUnEPOzbNoNei5uGgkk9LTk8ktjKyQ/formResponse?ifq&entry.********=";//umidade do solo
 String forms2 = "GET /forms/d/e/1FAIpQLSdz5nJ9VMvICIn8jXRHdUnEPOzbNoNei5uGgkk9LTk8ktjKyQ/formResponse?ifq&entry.********=";//umidade do ar
 String forms3 = "GET /forms/d/e/1FAIpQLSdz5nJ9VMvICIn8jXRHdUnEPOzbNoNei5uGgkk9LTk8ktjKyQ/formResponse?ifq&entry.********=";//temperaturado ar
 
-
+  /=================================================================================================================*/
 
 void setup() {
   Serial.begin (115200);
@@ -123,18 +125,22 @@ void setup() {
 
   dht.begin();
 
+  ativaIrrig();
 
 }
 
 void loop() {
   server.handleClient();
 
-
   ativaIrrig();
 
-  delay(tempoEntIrrigacao*60000);
+  delay(tempoEntIrrigacao * minuto);
 }
 
+void handle_NotFound() { //Função para lidar com o erro 404
+  server.send(404, "text/plain", "Não encontrado");
+
+}
 
 void handle_OnConnect() {
   temperatura = dht.readTemperature();  //Realiza a leitura da temperatura
@@ -196,95 +202,87 @@ String EnvioHTML(float Temperaturastat, float Umidadestat, float Humstat) { //Ex
 
 void getSoilData(void) {
   digitalWrite(vccSensor, HIGH);
-  millis(1000);
+
+  delay(1000);
 
   hum = (100 - (( analogRead(sensorSolo) - 550) / porcent));
 
   Serial.print("Umidade do solo");
   Serial.print(hum);
   Serial.print("%");
-  millis(1000);
+  delay(1000);
   digitalWrite(vccSensor, LOW);
   delay(10);
 
 
-    client.setInsecure();
+  client.setInsecure();
 
-    if (client.connect("docs.google.com", 443) == 1)//Tenta se conectar ao servidor do Google docs na porta 443 (HTTPS)
-    {
-      String toSend = forms1;//Atribuimos a String auxiliar na nova String que sera enviada
-      toSend += hum;//Adicionamos um valor aleatorio
-      toSend += "&submit=Submit HTTP/1.1";//Completamos o metodo GET para nosso formulario.
-      client.println(toSend);//Enviamos o GET ao servidor-
-      client.println("Host: docs.google.com");//-
-      client.println();//-
-      client.stop();//Encerramos a conexao com o servidor
-      Serial.println("Dados de umidade do solo enviados.");//Mostra no monitor que foi enviado
-    }
-    else {
-      Serial.println("Erro ao se conectar");//Se nao for possivel conectar no servidor, ira avisar no monitor.
+  if (client.connect("docs.google.com", 443) == 1)//Tenta se conectar ao servidor do Google docs na porta 443 (HTTPS)
+  {
+    String toSend = forms1;//Atribuimos a String auxiliar na nova String que sera enviada
+    toSend += hum;//Adicionamos um valor aleatorio
+    toSend += "&submit=Submit HTTP/1.1";//Completamos o metodo GET para nosso formulario.
+    client.println(toSend);//Enviamos o GET ao servidor-
+    client.println("Host: docs.google.com");//-
+    client.println();//-
+    client.stop();//Encerramos a conexao com o servidor
+    Serial.println("Dados de umidade do solo enviados.");//Mostra no monitor que foi enviado
+  } else {
+    Serial.println("Erro ao se conectar");//Se nao for possivel conectar no servidor, ira avisar no monitor.
 
-      delay(50);
-    }
-
-    if (client.connect("docs.google.com", 443) == 1)//Tenta se conectar ao servidor do Google docs na porta 443 (HTTPS)
-    {
-      String toSend = forms2;//Atribuimos a String auxiliar na nova String que sera enviada
-      toSend += umidade;//Adicionamos um valor aleatorio
-      toSend += "&submit=Submit HTTP/1.1";//Completamos o metodo GET para nosso formulario.
-      client.println(toSend);//Enviamos o GET ao servidor-
-      client.println("Host: docs.google.com");//-
-      client.println();//-
-      client.stop();//Encerramos a conexao com o servidor
-      Serial.println("Dados de umidade do ar enviados.");//Mostra no monitor que foi enviado
-    }
-    else {
-      Serial.println("Erro ao se conectar");//Se nao for possivel conectar no servidor, ira avisar no monitor.
-      delay(50);
-    }
-
-
-
-    if (client.connect("docs.google.com", 443) == 1)//Tenta se conectar ao servidor do Google docs na porta 443 (HTTPS)
-    {
-      String toSend = forms3;//Atribui a String auxiliar na nova String que sera enviada
-      toSend += temperatura;//Adicion um valor da temperatura
-      toSend += "&submit=Submit HTTP/1.1";//Completamos o metodo GET para nosso formulario.
-      client.println(toSend);//Envia o GET ao servidor-
-      client.println("Host: docs.google.com");//-
-      client.println();//-
-      client.stop();//Encerra a conexao com o servidor
-      Serial.println("Dados de temperatura enviados.");//Mostra no monitor que foi enviado
-    }
-    else {
-      Serial.println("Erro ao se conectar");//Se nao for possivel conectar no servidor, ira avisar no monitor.
-
-      delay(50);
-
-    }
+    delay(50);
   }
-  
+
+  if (client.connect("docs.google.com", 443) == 1)//Tenta se conectar ao servidor do Google docs na porta 443 (HTTPS)
+  {
+    String toSend = forms2;//Atribuimos a String auxiliar na nova String que sera enviada
+    toSend += umidade;//Adicionamos um valor aleatorio
+    toSend += "&submit=Submit HTTP/1.1";//Completamos o metodo GET para nosso formulario.
+    client.println(toSend);//Enviamos o GET ao servidor-
+    client.println("Host: docs.google.com");//-
+    client.println();//-
+    client.stop();//Encerramos a conexao com o servidor
+    Serial.println("Dados de umidade do ar enviados.");//Mostra no monitor que foi enviado
+  }
+  else {
+    Serial.println("Erro ao se conectar");//Se nao for possivel conectar no servidor, ira avisar no monitor.
+    delay(50);
+  }
+
+
+  if (client.connect("docs.google.com", 443) == 1)//Tenta se conectar ao servidor do Google docs na porta 443 (HTTPS)
+  {
+    String toSend = forms3;//Atribui a String auxiliar na nova String que sera enviada
+    toSend += temperatura;//Adicion um valor da temperatura
+    toSend += "&submit=Submit HTTP/1.1";//Completamos o metodo GET para nosso formulario.
+    client.println(toSend);//Envia o GET ao servidor-
+    client.println("Host: docs.google.com");//-
+    client.println();//-
+    client.stop();//Encerra a conexao com o servidor
+    Serial.println("Dados de temperatura enviados.");//Mostra no monitor que foi enviado
+  }
+  else {
+    Serial.println("Erro ao se conectar");//Se nao for possivel conectar no servidor, ira avisar no monitor.
+
+    delay(50);
+
+  }
 }
 
-void handle_NotFound() { //Função para lidar com o erro 404
-  server.send(404, "text/plain", "Não encontrado");
-
-}
-
-void ativaIrrig (void) {
+void ativaIrrig() {
   delay(100);
   digitalWrite(vccSensor, HIGH);
-  millis(2000);
+  delay(2000);
 
   while ((100 - (( analogRead(sensorSolo) - 550) / porcent)) <= valorMin ) {
-    millis(10);
+    delay(10);
     Serial.print("bomba ligada");
     digitalWrite(bomba, HIGH);
-    delay(tempoIrrigando*1000);
+    delay(tempoIrrigando * segundos);
     digitalWrite(bomba, LOW);
     digitalWrite(vccSensor, LOW);
     Serial.print("bomba desligada");
 
 
- 
+  }
 }
